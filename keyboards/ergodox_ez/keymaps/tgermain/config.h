8c60d0a83e7812300376143197b3c01c0364dc15
Timothée GERMAIN <timothee@lumapps.com>
2018-08-06 22:59:49 +0200

    wip, untested layout but it compile

---
+++ keyboards/ergodox_ez/keymaps/tgermain/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_TGERMAIN_H
+#define CONFIG_TGERMAIN_H
+
+#include "../../config.h"
+
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 1000
+
+#undef LEADER_TIMEOUT
+#define LEADER_TIMEOUT 1000
+
+#endif
